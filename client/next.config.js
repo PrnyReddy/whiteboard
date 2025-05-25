313c1114c242cd071e1807a654ba94c788b4e853
Pranay Reddy <prnyreddy111@gmail.com>
2025-05-25 13:38:17 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const isProduction = process.env.NODE_ENV === 'production';
-const repoName = 'whiteboard';
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProduction ? `/${repoName}` : '',
-  assetPrefix: isProduction ? `/${repoName}/` : '',
+  basePath: '',
+  assetPrefix:'',
   experimental: {
     appDir: true
   }
